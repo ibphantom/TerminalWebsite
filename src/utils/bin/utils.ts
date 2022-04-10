861bec9b48753140e960767e580aa9701e2b2dc1
ibphantom <62777429+ibphantom@users.noreply.github.com>
2022-04-10 01:53:18 -0400

    Update utils.ts

--- src/utils/bin/utils.ts
+++ src/utils/bin/utils.ts
@@ -44,9 +44,6 @@ export const emacs = async (args?: string[]): Promise<string> => {
 };
 
 export const sudo = async (args?: string[]): Promise<string> => {
-  export const superuser = async (args?: string[]): Promise<string> => {
-    return `'WOW' - Owen Wilson`;
-    };
   window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
 
   return `Permission denied: unable to run the command '${args[0]}' as root.`;
