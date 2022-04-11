bc84c61a053c4ffcc8a81b9b336174ef380f6f8e
ibphantom <62777429+ibphantom@users.noreply.github.com>
2022-04-11 19:54:50 -0400

    Update [city].ts

--- src/pages/api/weather/[city].ts
+++ src/pages/api/weather/[city].ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default async function handler(req, res) {
   const { city } = req.query;
-  const { data } = await axios.get(`https://wttr.in/${city}?Fu2`);
+  const { data } = await axios.get(`https://wttr.in/${city}?Fu2p`);
 
   res.status(200).json(data);
 }
