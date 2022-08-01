3269a63bbb0701c6c355979f6bec08578b158cbb
ibphantom <62777429+ibphantom@users.noreply.github.com>
2022-07-31 20:51:35 -0400

    Delete src/pages/api/weather directory

--- src/pages/api/weather/[city].ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default async function handler(req, res) {
-  const { city } = req.query;
-  const { data } = await axios.get(`https://wttr.in/${city}?Fu`);
-
-  res.status(200).json(data);
-}
