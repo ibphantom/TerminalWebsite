3b2f94083997aa16165843fe4ac79ae001902111
ibphantom <62777429+ibphantom@users.noreply.github.com>
2022-07-31 20:37:08 -0400

    Update utils.ts

--- src/utils/bin/utils.ts
+++ src/utils/bin/utils.ts
@@ -1,8 +1,8 @@
-import * as bin from './index';
 import packageJson from '../../../package.json';
+import * as bin from './index';
 
 export const help = async (args: string[]): Promise<string> => {
-  const commands = Object.keys(bin).sort().join('\n');
+  const commands = Object.keys(bin).sort().join(', ');
 
   return `Available commands:\n${commands}\n\n[tab]\t trigger completion.\n[ctrl+l] clear terminal.\n[ctrl+c] cancel command.`;
 };
@@ -12,7 +12,7 @@ export const echo = async (args: string[]): Promise<string> => {
 };
 
 export const whoami = async (args: string[]): Promise<string> => {
-  return 'Yeah... Who are you..?';
+  return 'guest';
 };
 
 export const date = async (args: string[]): Promise<string> => {
@@ -116,9 +116,10 @@ export const banner = (args?: string[]): string => {
 v${packageJson.version}
 
 Type 'help' to see list of available commands.
-
 --
-This project is open-source 🎉 type 'repo' to check out the repository.
+The project is open-source 🎉 type 'repo' to check out the repository.
+New 🎉: Try out the new 'theme' command. See all available themes <a href="https://github.com/m4tt72/terminal/tree/master/docs/themes">in the docs</a>.
+New 🎉: New command 'neofetch', for you linux.
 --
 `;
 };
