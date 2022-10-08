7597e376a6c8a6cff406e6146a5b6b3503c0fed5
ibphantom <62777429+ibphantom@users.noreply.github.com>
2022-10-08 16:46:39 -0400

    Update randomnum.ts

--- src/utils/bin/randomnum.ts
+++ src/utils/bin/randomnum.ts
@@ -1,16 +1,8 @@
 export const randomnum = async (args: string[]): Promise<string> => {
-  function getRandomExcept(min, max, except) {
-  except.sort(function(a, b) {
-    return a - b;
-  });
-  var random = Math.floor(Math.random() * (max - min + 1 - except.length)) + min;
-  var i;
-  for (i = 0; i < except.length; i++) {
-    if (except[i] > random) {
-      break;
-    }
-    random++;
+  const rndInt = randomIntFromInterval(99999, 1000000)
+
+function randomIntFromInterval(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min)
   }
-  return random;
-  }
-}
+  return rndInt
+};
