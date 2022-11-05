b99256a809f3b60bcc492522499fd4da7f59d4ba
ibphantom <62777429+ibphantom@users.noreply.github.com>
2022-11-05 00:45:22 -0400

    Update utils.ts

--- src/utils/bin/utils.ts
+++ src/utils/bin/utils.ts
@@ -44,11 +44,7 @@ export const emacs = async (args?: string[]): Promise<string> => {
 };
 
 export const marilyn = async (args?: string[]): Promise<string> => {
-  return `Hey, Marilyn! I love you so very much.💞`;
-};
-
-export const cutecouple = async (args?: string[]): Promise<string> => {
-  window.open('https://gui.zachk.dev/index2.html', '_blank');
+  return `Hey, Marilyn! I love you so very much💞`;
 };
 
 export const sudo = async (args?: string[]): Promise<string> => {
