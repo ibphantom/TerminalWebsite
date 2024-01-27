ff2c48670f12d6f2f0dfcc2702640713d76284aa
Zach <62777429+ibphantom@users.noreply.github.com>
2024-01-27 10:39:57 -0500

    Delete src/utils/bin directory

--- src/utils/bin/randomnum.ts
+++
@@ -1,8 +0,0 @@
-export const randomnum = async (args: number[]): Promise<number> => {
-  const rndInt = randomIntFromInterval(99999, 1000000)
-
-function randomIntFromInterval(min, max) { // min and max included 
-  return Math.floor(Math.random() * (max - min + 1) + min)
-  }
-  return rndInt
-};
