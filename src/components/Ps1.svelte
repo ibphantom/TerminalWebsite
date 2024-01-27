943824e3140663a7bd10cad260aadc9e81b5c3a1
Zach <62777429+ibphantom@users.noreply.github.com>
2024-01-27 17:46:09 -0500

    Update Ps1.svelte

--- src/components/Ps1.svelte
+++ src/components/Ps1.svelte
@@ -4,8 +4,8 @@
 </script>
 
 <h1 class="font-bold flex">
-  <span style={`color: ${$theme.yellow};`}>{guestName}</span>
+  <span style={`color: ${$theme.yellow};`}>up51d3d0wn</span>
   <span style={`color: ${$theme.white}`}>@</span>
   <span style={`color: ${$theme.green}`}>{hostname}</span>
-  <span style={`color: ${$theme.white}`}>:~$</span>
+  <span style={`color: ${$theme.white}`}>:$:</span>
 </h1>
