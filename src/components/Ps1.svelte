193ff5193d62f64f128efe76d86c0ddf6b7b18d2
Zach <62777429+ibphantom@users.noreply.github.com>
2024-01-27 18:28:06 -0500

    Update Ps1.svelte

--- src/components/Ps1.svelte
+++ src/components/Ps1.svelte
@@ -1,10 +1,10 @@
 <script>
-  import { theme } from '../stores/theme';
+  import { theme, username } from '../stores/theme';
   let hostname = window.location.hostname;
 </script>
 
 <h1 class="font-bold flex">
-  <span style={`color: ${$theme.yellow};`}>up51d3d0wn</span>
+  <span style={`color: ${$theme.yellow};`}>{$username}</span>
   <span style={`color: ${$theme.white}`}>@</span>
   <span style={`color: ${$theme.green}`}>{hostname}</span>
   <span style={`color: ${$theme.white}`}>~$</span>
