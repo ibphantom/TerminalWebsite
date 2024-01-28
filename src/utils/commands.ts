1471b44cfe469a239e66b6e64da3dc72cefa8511
Zach <62777429+ibphantom@users.noreply.github.com>
2024-01-27 20:39:27 -0500

    Update commands.ts

--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -4,54 +4,21 @@ import { history } from '../stores/history';
 import { theme } from '../stores/theme';
 
 const hostname = window.location.hostname;
-let isAuthenticated = false;
 
 export const commands: Record<string, (args: string[]) => Promise<string> | string> = {
   help: () => 'Available commands: ' + Object.keys(commands).join(', '),
-  hostname: () => isAuthenticated ? hostname : 'Authentication required',
+  hostname: () => hostname,
+  whoami: () => 'guest',
+  date: () => new Date().toLocaleString(),
+  vi: () => `why use vi? try 'emacs'`,
+  vim: () => `why use vim? try 'emacs'`,
+  emacs: () => `why use emacs? try 'vim'`,
+  echo: (args: string[]) => args.join(' '),
+  sudo: (args: string[]) => {
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
 
-  whoami: () => {
-    if (isAuthenticated) {
-      return 'SUDO';
-    } else {
-      const storedUsername = localStorage.getItem('guestName');
-      if (storedUsername) {
-        username.set(storedUsername);
-        return storedUsername;
-      } else {
-        const enteredName = prompt('Yeah, who are you? Enter your name:');
-        if (enteredName) {
-          localStorage.setItem('guestName', enteredName);
-          username.set(enteredName);
-          return enteredName;
-        } else {
-          return 'Authentication required';
-        }
-      }
-    }
+    return `Permission denied: unable to run the command '${args[0]}' as root.`;
   },
-
-  date: () => isAuthenticated ? new Date().toLocaleString() : 'Authentication required',
-  vi: () => isAuthenticated ? `why use vi? try 'emacs'` : 'Authentication required',
-  vim: () => isAuthenticated ? `why use vim? try 'emacs'` : 'Authentication required',
-  emacs: () => isAuthenticated ? `why use emacs? try 'vim'` : 'Authentication required',
-  echo: (args: string[]) => isAuthenticated ? args.join(' ') : 'Authentication required',
-  sudo: async (args: string[]) => {
-    // Simulate password prompt with masked input
-    const enteredPassword = prompt('Enter your password:', { type: 'password' });
-
-    // Replace the condition with your actual password validation logic
-    if (enteredPassword === 'DoubleDown!!') {
-      isAuthenticated = true;
-      return `Authentication successful. You can now run '${args.join(' ')}' as root.`;
-    } else {
-      isAuthenticated = false;
-      return 'Authentication failed. Permission denied.';
-    }
-  },
-};
-
-
   theme: (args: string[]) => {
     const usage = `Usage: theme [args].
     [args]:
@@ -96,46 +63,41 @@ export const commands: Record<string, (args: string[]) => Promise<string> | stri
       }
     }
   },
-
-  repo; () => {
+  repo: () => {
     window.open(packageJson.repository.url, '_blank');
 
     return 'Opening repository...';
   },
-  clear; () => {
+  clear: () => {
     history.set([]);
 
     return '';
   },
-  email; () => {
+  email: () => {
     window.open(`mailto:${packageJson.author.email}`);
 
     return `Opening mailto:${packageJson.author.email}...`;
   },
-  donate; () => {
+  donate: () => {
     window.open(packageJson.funding.url, '_blank');
 
     return 'Opening donation url...';
   },
-  weather; async (args: string[]) => {
+  weather: async (args: string[]) => {
     const city = args.join('+');
 
     if (!city) {
-      return 'Usage: weather [city]. Example: weather Chicago';
+      return 'Usage: weather [city]. Example: weather Brussels';
     }
 
     const weather = await fetch(`https://wttr.in/${city}?ATm`);
 
     return weather.text();
   },
-  exit; () => {
-    isAuthenticated = false; // Reset authentication upon exit
-    if (window.confirm('Are you sure you want to exit?')) {
-      closeTabBasedOnOS(); // Close the tab if not authenticated
-    }
-    return 'Exiting...';
+  exit: () => {
+    return 'Please close the tab to exit.';
   },
-  curl; async (args: string[]) => {
+  curl: async (args: string[]) => {
     if (args.length === 0) {
       return 'curl: no URL provided';
     }
@@ -151,7 +113,7 @@ export const commands: Record<string, (args: string[]) => Promise<string> | stri
       return `curl: could not fetch URL ${url}. Details: ${error}`;
     }
   },
-  banner; () => `
+  banner: () => `
 ███████╗ █████╗  ██████╗██╗  ██╗██╗  ██╗   ██████╗ ███████╗██╗   ██╗
 ╚══███╔╝██╔══██╗██╔════╝██║  ██║██║ ██╔╝   ██╔══██╗██╔════╝██║   ██║
   ███╔╝ ███████║██║     ███████║█████╔╝    ██║  ██║█████╗  ██║   ██║
@@ -160,14 +122,4 @@ export const commands: Record<string, (args: string[]) => Promise<string> | stri
 
 Type 'help' to see list of available commands.
 `,
-
-// Function to close the tab based on OS
-function closeTabBasedOnOS() {
-  if (navigator.userAgent.match(/(Chrome|Firefox)/)) {
-    window.close();
-  } else if (navigator.appVersion.indexOf('Mac') !== -1) {
-    // macOS-specific closing logic (e.g., using AppleScript)
-  } else {
-    // Other OS-specific closing logic
-  }
 };
