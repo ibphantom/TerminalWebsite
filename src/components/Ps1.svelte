551efede4779d6f139d0428f5f3cf69e95626d92
Zach <62777429+ibphantom@users.noreply.github.com>
2024-01-27 20:14:36 -0500

    Update Ps1.svelte

--- src/components/Ps1.svelte
+++ src/components/Ps1.svelte
@@ -1,23 +1,11 @@
-// TerminalWebsite/src/components/Ps1.svelte
 <script>
-  import { theme, username } from '../stores/theme';
+  import { theme } from '../stores/theme';
   let hostname = window.location.hostname;
-
-  // Subscribe to the username store
-  let unsubscribe = username.subscribe(value => {
-    $username = value;
-    console.log('Username updated:', value);
-  });
-
-  // Make sure to unsubscribe when the component is destroyed
-  onDestroy(() => {
-    unsubscribe();
-  });
 </script>
 
 <h1 class="font-bold flex">
-  <span style={`color: ${$theme.yellow};`}>{$username}</span>
+  <span style={`color: ${$theme.yellow};`}>guest</span>
   <span style={`color: ${$theme.white}`}>@</span>
   <span style={`color: ${$theme.green}`}>{hostname}</span>
-  <span style={`color: ${$theme.white}`}>~$</span>
+  <span style={`color: ${$theme.white}`}>:~$</span>
 </h1>
