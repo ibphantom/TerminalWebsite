5a4acc22808f559f3fbcc89b84ac84714ee6a8ed
Zach <62777429+ibphantom@users.noreply.github.com>
2024-01-27 20:14:15 -0500

    Update theme.ts

--- src/stores/theme.ts
+++ src/stores/theme.ts
@@ -1,26 +1,15 @@
-<!-- TerminalWebsite/src/components/Ps1.svelte -->
-<script>
-  import { theme } from '../stores/theme';
-  let hostname = window.location.hostname;
+import { writable } from 'svelte/store';
+import themes from '../../themes.json';
+import type { Theme } from '../interfaces/theme';
 
-  // Destructure the username store from the theme store
-  let { subscribe, set } = theme;
-  let $username = ''; // Initialize $username with an empty string
+const defaultColorscheme: Theme = themes.find((t) => t.name === 'GruvboxDark')!;
 
-  // Subscribe to the theme store and extract the username
-  let unsubscribe = subscribe(value => {
-    $username = value.username || ''; // Extract the username property from the theme
-  });
+export const theme = writable<Theme>(
+  JSON.parse(
+    localStorage.getItem('colorscheme') || JSON.stringify(defaultColorscheme),
+  ),
+);
 
-  // Make sure to unsubscribe when the component is destroyed
-  onDestroy(() => {
-    unsubscribe();
-  });
-</script>
-
-<h1 class="font-bold flex">
-  <span style={`color: ${$theme.yellow};`}>{$username}</span>
-  <span style={`color: ${$theme.white}`}>@</span>
-  <span style={`color: ${$theme.green}`}>{hostname}</span>
-  <span style={`color: ${$theme.white}`}>~$</span>
-</h1>
+theme.subscribe((value) => {
+  localStorage.setItem('colorscheme', JSON.stringify(value));
+});
